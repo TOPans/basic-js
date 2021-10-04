3ba9bb3eee74d28ca7889b85a4229ec30764233b
TOPans <51285596+TOPans@users.noreply.github.com>
2021-10-04 22:01:19 +0300

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,21 +1,42 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  let result = "";
+  const {
+    repeatTimes = 1,
+    separator = "+",
+    addition = "",
+    additionRepeatTimes = 1,
+    additionSeparator = "|",
+  } = options;
+  for (let i = 0; i < repeatTimes; i++) {
+    result += str;
+    if (additionRepeatTimes) {
+      for (let j = 0; j < additionRepeatTimes; j++) {
+        result += addition;
+        if (j !== additionRepeatTimes - 1) {
+          result += additionSeparator;
+        }
+      }
+    }
+    if (i !== repeatTimes - 1) {
+      result += separator;
+    }
+  }
+  return result;
 }
